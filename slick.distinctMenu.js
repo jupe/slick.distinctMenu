5ebe0c3565669d86e91280a86d7c8b10f24c2098
Jussi Vatjus-Anttila <jussiva@gmail.com>
2013-09-26 19:45:22 +0300

    Update slick.distinctMenu.js
    several improvements.

--- slick.distinctMenu.js
+++ slick.distinctMenu.js
@@ -18,26 +18,50 @@
       url: '/items',        //default url for query distinct arrays (JSON format)
       urlParameters: null,  //extra parameters for url string
       exclude: [],          //exclude column id's
-      
       doUrl: doUrl,
       doCondition: doCondition,
       doFilter: doFilter,
       onReady: onReady,
-      
+      selectIcon: "/js/vendor/SlickGrid/images/tick.png",
+      condition: {$and: []}
     };
-  
+    function condition(field, condition){
+      var obj = {}
+      if( field === false ){
+        options.condition['$and'] = [];
+        for (var i=1;i<=options.columns.length;i++){
+          options.condition['$and'].push({});
+        }
+      } else if(field && condition ) {
+        var col = getColumnIndexByField(field);
+        obj[field] = condition;
+        options.condition['$and'][col] = condition;
+      }
+      return options.condition;
+    }
     function init(grid) {
       options = $.extend(true, {}, _defaults, options);
       _grid = grid;
+      for (var i=1;i<=options.columns.length;i++){
+        options.condition['$and'].push({});
+      }
       options.headerMenu.onCommand.subscribe(onCommand);
       console.log('distinctMenu initialized');
     }
+    function getColumnIndexByField(field)
+    {
+      var i;
+      for(i=0;i<options.columns.length;i++){
+        if( options.columns[i].field == field ) return i;
+      } return;
+    }
     function onCommand(e, args) {
       if( args.command == options.command ){
-        
         //options.exclude = args.column.field
+        setSelection(args.column, args.item);
+        condition(args.column.field, args.item.condition);
         if( args.item.condition ) {
-          options.doFilter( args.column.field, args.item.condition );
+          options.doFilter( args.column.field, condition() );
         }
       }
     }
@@ -45,6 +69,20 @@
       _handler.unsubscribeAll();
       console.log('distinctMenu destroyed');
       //$(document.body).unbind("mousedown", handleBodyMouseDown);
+    }
+    function setSelection(column, item){
+      $.extend(true, column, {header: {menu: {items: []}}})
+      var items = column.header.menu.items;
+      items.forEach( function( value ) {
+        if( value.command == options.command ){
+          //belong to distinct menus
+          if( value.title === item.title ){
+            value.iconImage = options.selectIcon;
+          } else {
+            value.iconImage = false;
+          }
+        }
+      });
     }
     function onReady(columns){
       _grid.setColumns(columns);
@@ -63,7 +101,6 @@
     function doCondition(field, value){
       var cond = {}
       cond[field] = value;
-      console.log('generated condition: '+JSON.stringify(cond) );
       return cond;
     }
     
@@ -74,7 +111,6 @@
           updateDistinct(items, i+1, callback);
           return;
         }
-        
         var url = false;
         if( typeof(options.doUrl)=='function'){
           url = options.doUrl(items[i].field, options.condition);
@@ -83,8 +119,8 @@
           //doError({error: 'doUrl function missing!');
           return;
         }
-        console.log(url);
-        $.getJSON( url, options.urlParameters
+        console.log(url); //debug print
+        $.getJSON( url, $.extend({json: true}, options.urlParameters)
         ).done(function( json  ) {
             if( json.length ){
               setColumnMenuFilters(i, items[i], json);
@@ -94,76 +130,55 @@
             updateDistinct(items, i+1, callback);
         }).fail(function(jqxhr, textStatus, error) {
           var err = textStatus + ", " + error;
-          console.log( "Request Failed: " + err );
+          console.error( "Request Failed: " + err );
           updateDistinct(items, i+1, callback);
         });
       } else {
         callback(options.columns);
       }
     }
-    function update(){
+    function updateMenus(){
       updateDistinct(options.columns, 0, options.onReady);
     }
     function doFilter(field, condition){
       console.log('Filteringing with condition: '+JSON.stringify(condition) );
     }
+    function removeFunction (myObjects,prop,valu)
+    {
+      return myObjects.filter(function (val) {
+          return val[prop] !== valu;
+      });
+    }
     function setColumnMenuFilters(coll, item, distinct){
       console.log('setColumnMenuFilters: '+distinct);
-      if( options.columns[coll].header && 
-          options.columns[coll].header.menu &&
-          options.columns[coll].header.menu.items ) {
-        console.log('loop cols');
+      $.extend(true, options.columns[coll], {header: {menu: {items: []}}})
+      
+      //remove old items (only where command is "distinct")
+      var items = options.columns[coll].header.menu.items;
+      options.columns[coll].header.menu.items = removeFunction(items, 'command', options.command);
+      var items = options.columns[coll].header.menu.items;
+      //push "all" button
+      items.push( {           
+        command: options.command,
+        title: 'All',
+        condition: {},
+        iconImage: options.selectIcon,
+      });
+      //push individual items
+      distinct.forEach( function( value ) {
+        console.log('addItem: '+value);
+        items.push({
+          command: options.command,
+          title: value,
+          condition: options.doCondition(item.field, value),
+        });     
+      });
         
-        var items = options.columns[coll].header.menu.items;
-        //items = items.slice(0,1);
-        distinct.forEach( function( value ) {
-          console.log('addItem: '+value);
-          /*
-            var list = []
-            var menu = options.columns[items[i].column].header.menu.items;
-            options.columns[items[i].column].header.menu.items = menu;
-            
-            //
-            $.each(ret, function(j, item){
-              if( items[i].getFunc ) {
-                item = items[i].getFunc(item);
-                if( !item ) return;
-              }
-              if( list.indexOf(item)==-1 )
-                list.push(item);
-            });
-            $.each(list, function(j, item){
-              var cond = {}
-              
-              if( items[i].condFunc ) {
-                cond = items[i].condFunc(item);
-              }
-              else {
-                cond[items[i].field] = item;
-              }
-              columns[items[i].column].header.menu.items.push(
-              {
-                command: items[i].command,
-                title: item,
-                condition: cond
-              });     
-              
-            });
-            updateFilter(items, i+1, callback);
-          });*/
-          items.push(
-            {
-              command: options.command,
-              title: value,
-              condition: doCondition(item.field, value)
-            });     
-        });
-      }
     }
     $.extend(this, {
       "init": init,
       "destroy": destroy,
-      "update": update
+      "update": updateMenus,
       //"onBeforeMenuShow": new Slick.Event(),
       //"onCommand": new Slick.Event()
     });
